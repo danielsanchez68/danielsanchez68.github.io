2e67884a3d838c81486b6e43f7bbeda50bbaf009
danielsanchez68 <danielsanchez68@hotmail.com>
2019-09-28 20:53:30 -0300

    v11

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ if ('serviceWorker' in navigator) {
         console.warn('Error whilst registering service worker', err);
     });
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
