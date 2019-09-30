6c6bb83b6a40f0f7d884d87cdcf991635bb6f0fa
danielsanchez68 <danielsanchez68@hotmail.com>
2019-09-30 20:02:54 -0300

    Super Lista V0.0.1

--- js/main.js
+++ js/main.js
@@ -67,13 +67,8 @@ document.getElementById('agregar-producto').addEventListener('click', () => {
     renderLista()
 })
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(function(reg) {
-        console.log('Successfully registered service worker', reg);
-    }).catch(function(err) {
-        console.warn('Error whilst registering service worker', err);
-    });
-}
+
+
 
 
 renderLista()
