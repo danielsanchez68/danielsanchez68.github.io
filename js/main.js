c45b4e85db985e50ec3341ca027184cc631a8fce
danielsanchez68 <danielsanchez68@hotmail.com>
2019-09-30 06:55:04 -0300

    va-2

--- js/main.js
+++ js/main.js
@@ -68,7 +68,13 @@ document.getElementById('agregar-producto').addEventListener('click', () => {
 })
 
 
-
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js').then(function(reg) {
+        console.log('Successfully registered service worker', reg);
+    }).catch(function(err) {
+        console.warn('Error whilst registering service worker', err);
+    });
+}
 
 
 renderLista()
