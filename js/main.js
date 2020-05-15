156a58ab22f1c6a0ffcca6f1581402904cb5db8a
danielsanchez68 <danielsanchez68@hotmail.com>
2020-05-15 14:09:11 -0300

    v0.0.5

--- js/main.js
+++ js/main.js
@@ -123,20 +123,7 @@ function renderLista() {
     crearLista = false;
 }
 
-function registrarServiceWorker() {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register('./sw.js').then(function(reg) {
-                console.log('Successfully registered service worker', reg);
-            }).catch(function(err) {
-                console.warn('Error whilst registering service worker', err);
-            });
-        })
-    }
-}
-
 function start() {
-    registrarServiceWorker()
     configurarListeners()
     renderLista()
 }
